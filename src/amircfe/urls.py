bb1305f51b1090e5b459292b3535f9b67f255e98
amirka <amirahmet897@gmail.com>
2026-07-10 18:13:16 +0500

    Prep for railway

--- src/amircfe/urls.py
+++ src/amircfe/urls.py
@@ -21,7 +21,7 @@ from .views import home_view, about_view
 
 urlpatterns = [
     path('hello-world/', home_view),
-    path("/about", about_view),
+    path("about/", about_view),
     path('admin/', admin.site.urls),
     path('home_page/', home_view)
 ]
