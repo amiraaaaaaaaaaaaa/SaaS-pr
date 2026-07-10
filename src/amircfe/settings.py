590b713a9fc91849926d71ad3a4e94ff3a996baf
amirka <amirahmet897@gmail.com>
2026-07-10 19:20:42 +0500

    Prep for railway

--- src/amircfe/settings.py
+++ src/amircfe/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)o6fl6&c#+cjhasg#j4!dn&rlws&f(qc_2#0w*aok$vc!3o0&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+print("DEBUG", DEBUG)
+
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
