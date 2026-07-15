35ec174b7ef78afe21ea3dca8d183e913b7db9ff
amirka <amirahmet897@gmail.com>
2026-07-15 19:01:35 +0500

    updated vendr

--- src/amircfe/settings.py
+++ src/amircfe/settings.py
@@ -155,6 +155,14 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'local-cdn'
 
+
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
